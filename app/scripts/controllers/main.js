63f7b92b6dd3df5b11e9bfeda72cf36919992ef6
taqgit <tariqueansari@Tariques-MacBook-Pro.local>
2015-02-19 21:42:59 -0500

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carsureApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the carsureApp
+ */
+angular.module('carsureApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
