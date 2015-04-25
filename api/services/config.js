e7a191924b687ca082fa0e013a78e41f77f7b1c4
taqgit <tariqueansari@Tariques-MacBook-Pro.local>
2015-04-25 03:25:10 -0400

    root

--- api/services/config.js
+++ api/services/config.js
@@ -19,7 +19,7 @@ console.log('rootPath --------------------    ' + rootPath);
 module.exports = {
     EMAIL_SECRET: 'somesecret',
     SMTP_PASS: '0ca0b85f08e4510f90ffed24bb4c6251',
-    APP_URL: 'http://localhost:9000/',
+    APP_URL: 'http://hidden-falls-3003.herokuapp.com/',
     FACEBOOK_SECRET: '8cdf109e45e5aa32f61c8b707207296d',
     GOOGLE_SECRET: 'VOfh6_KbjBWX12Q3hFggAgeE',
     rootPath: rootPath
